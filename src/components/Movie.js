d80d856057b1763e0f6019df00dee8fea963f4a3
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-10 16:58:55 +0200

    conditional rendering 1

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,20 @@
 function Movie(props){
+
+    let content;
+
+    if(props.rating > 8) {
+        content = <span>Recommended</span>
+    }
+
     return(
         <div>
         <h3>{props.id}.Title: {props.title}</h3>
         <p>Year: {props.year}</p>
         <p>Genre: {props.genres}</p>
         <img src={props.imgURL} alt="image"></img>
-        <p>Rating : {props.rating}/10</p>
+        <p>Rating : {props.rating}/10 <br/>
+            {content}
+       </p>
         </div>
     )
 }
