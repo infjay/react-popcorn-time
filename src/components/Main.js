b6a7fc5dac97086a10fb565db256e451e4b2d026
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-10 17:42:58 +0200

    Filter function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,46 @@
-import movies from '../data/movies.json'
-import Movie from './Movie';
+import { useState } from "react";
+import moviesArr from "../data/movies.json";
+import Movie from "./Movie";
 
 function Main() {
 
+    const [movies, setMovies] = useState(moviesArr);
 
 
-  return (
-    <>
+    const filterTopRatedOnly = () => {
+        
+        // option 1: we pass the new value
+        // setMovies(newListOfMovies);
+        
+        // option 2: we pass a function (functional update)
+        setMovies( (prevValue) => {
+            const newListOfMovies = prevValue.filter( (movie) => {
+                return movie.rating > 8;
+            });
+            return newListOfMovies;
+        });
 
+    }
 
-      {movies.map((element)=>{
-        return <Movie  
-        key={element.id} 
-        id={element.id} 
-        title={element.title}
-        year={element.year} 
-        genre={element.genres}
-        rating={element.rating} />
-      }) }
+    return (
+        <>
 
+            <div className="controls">
+                <button onClick={filterTopRatedOnly}>Display Top Rated Only</button>
+            </div>
 
-
-    </>
-  );
+            {movies.map((element) => {
+                return (
+                    <Movie 
+                        key={element.id} 
+                        title={element.title} 
+                        year={element.year} 
+                        rating={element.rating} 
+                        />
+                    )
+            })}
+        </>
+    );
 }
 
 export default Main;
